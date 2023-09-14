8beb9cc325dd012b2f4f055a5ccbafd5603054dd
Replit user <>
2023-09-14 05:02:40 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -81,7 +81,7 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
   if(tree->root==NULL){
     return NULL;
   }
-  TreeNode* posicion=tree->root;
+  TreeNode* posicion=tree->current;
   while(posicion !=NULL){
     int resultado =tree->lower_than(key,posicion->pair->key);
     if(resultado==0){
